ea83c31a6b669dc0f170515279b3d3ed00793b33
schluis <luis.scheuch@posteo.de>
2024-04-15 09:01:35 +0200

    starship conda config

--- fish/config.fish
+++ fish/config.fish
@@ -14,18 +14,3 @@ alias l="ll"
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 
-
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /usr/bin/conda
-    eval /usr/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/usr/etc/fish/conf.d/conda.fish"
-        . "/usr/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/usr/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
