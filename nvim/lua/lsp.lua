d649e565634989bd32222c99f90ba258c98b63e9
schluis <luis.scheuch@posteo.de>
2024-04-22 08:26:52 +0200

    add plugin

---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,23 @@
+return {
+  -- handlers = {
+  --   rust_analyzer = function(_, opts) require("rust-tools").setup { server = opts } end,
+  -- },
+  -- mappings = {
+  --   n = {
+  --     ["<leader>a"] = {
+  --       function() vim.lsp.buf.code_action() end,
+  --       desc = "LSP code action",
+  --     },
+  --   },
+  --   v = {
+  --     ["<leader>a"] = {
+  --       function() vim.lsp.buf.code_action() end,
+  --       desc = "LSP code action",
+  --     },
+  --   },
+  -- },
+  -- servers = {
+  --   "rust_analyzer",
+  --   "clangd",
+  -- },
+}
--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -90,7 +90,7 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
@@ -100,7 +100,7 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
@@ -108,42 +108,26 @@ return {
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+          -- don't add a pair if the next character is %
+              :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+              :with_pair(
+                cond.not_before_regex("xxx", 3)
+              )
+          -- don't move right when repeat character
+              :with_move(cond.none())
+          -- don't delete if the next character is xx
+              :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+              :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+              :with_cr(cond.none()),
         },
         -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
       )
     end,
   },
-
-  -- {
-  --   "hrsh7th/nvim-cmp",
-  --   config = function()
-  --     local cmp = require "cmp"
-  --
-  --     cmp.setup {
-  --       snippet = {
-  --         expand = function(args) require("luasnip").lsp_expand(args.body) end,
-  --       },
-  --       -- mapping = {
-  --       --   ["<Tab>"] = nil,
-  --       -- },
-  --     }
-  --   end,
-  -- },
 
   {
     "kaarmu/typst.vim",
@@ -164,5 +148,13 @@ return {
   {
     "lukoshkin/auenv.nvim",
     lazy = false,
+  },
+  {
+    "2kabhishek/co-author.nvim",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = { "CoAuthor" },
   },
 }
