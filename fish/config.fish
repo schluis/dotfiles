8b934e84ea8b9e855adde25f423b32b449e08282
schluis <luis.scheuch@posteo.de>
2024-06-16 21:50:02 +0200

    update gitconfig

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+export PATH="/root/.local/bin:$PATH"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
@@ -9,8 +11,9 @@ end
 # Aliases
 alias ls="ls -G"
 alias l="ll"
-
+alias jupyter-notebook="~/.local/bin/jupyter-notebook --no-browser"
+alias ssh='ssh.exe'
+alias ssh-add='ssh-add.exe'
 
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
-
