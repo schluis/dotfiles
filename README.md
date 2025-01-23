8e286b9f468087d739769b92cfd3c030b11343ed
schluis <luis.scheuch@posteo.de>
2025-01-23 16:45:15 +0100

    more settings i guess

--- README.md
+++
@@ -1,2 +0,0 @@
-# dotfiles
-Some of my dotfiles
--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -80,7 +80,7 @@ return {
   {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.luasnip" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom luasnip configuration such as filetype extend or custom snippets
       local luasnip = require "luasnip"
       luasnip.filetype_extend("javascript", { "javascriptreact" })
@@ -90,7 +90,7 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
@@ -98,20 +98,20 @@ return {
       npairs.add_rules(
         {
           Rule("$", "$", { "tex", "latex" })
-            -- don't add a pair if the next character is %
-            :with_pair(cond.not_after_regex "%%")
-            -- don't add a pair if  the previous character is xxx
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            -- don't move right when repeat character
-            :with_move(cond.none())
-            -- don't delete if the next character is xx
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_del(cond.not_after_regex "xx")
-            -- disable adding a newline when you press <cr>
-            :with_cr(cond.none()),
+          -- don't add a pair if the next character is %
+              :with_pair(cond.not_after_regex "%%")
+          -- don't add a pair if  the previous character is xxx
+              :with_pair(
+                cond.not_before_regex("xxx", 3)
+              )
+          -- don't move right when repeat character
+              :with_move(cond.none())
+          -- don't delete if the next character is xx
+              :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+              :with_del(cond.not_after_regex "xx")
+          -- disable adding a newline when you press <cr>
+              :with_cr(cond.none()),
         },
         -- disable for .vim files, but it work for another filetypes
         Rule("a", "a", "-vim")
@@ -134,10 +134,12 @@ return {
     "github/copilot.vim",
     lazy = false,
   },
+
   {
     "lukoshkin/auenv.nvim",
     lazy = false,
   },
+
   {
     "2kabhishek/co-author.nvim",
     dependencies = {
@@ -146,6 +148,7 @@ return {
     },
     cmd = { "CoAuthor" },
   },
+
   {
     "akinsho/nvim-toggleterm.lua",
     config = require("toggleterm").setup {
@@ -153,6 +156,7 @@ return {
       persist_mode = true,
     },
   },
+
   {
     "ranjithshegde/ccls.nvim",
     lazy = false,
